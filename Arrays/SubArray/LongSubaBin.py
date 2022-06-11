4670aa9ad6e3c8c3dd6bf2b32da8aa4f401df2f7
GunalanD95 <guna19may2015@gmail.com>
2022-06-11 07:53:45 +0530

    Subarray:Problem

--- Arrays/SubArray/LongSubaBin.py
+++ Arrays/SubArray/LongSubaBin.py
@@ -7,60 +7,52 @@ Find and return the length of the longest consecutive 1’s that can be achieved
 import sys
 
 def CountLongSubArrayB(A):
-    A = list(A)
-    N = len(A)
+        A = list(A)
+        N = len(A) 
+        res_len = -sys.maxsize - 1
+        st_indx = 0
+        end_indx  = st_indx + 1
+        e1 = 0 
+        res = 0
+        no_1s = 0
+        # count the number of 1s in the array
+        for i in range(N):                                   # loop through the array to count the number of 1s
+            if A[i] == '1':
+                no_1s += 1
 
-
-    
-    res_len = -sys.maxsize - 1
-    st_indx = 0
-    end_indx  = st_indx + 1
-    s1 , e1 = 0 , 0
-    res = 0
-    no_1s = 0
-    # count the number of 1s in the array
-    for i in range(N):
-        if A[i] == '1':
-            no_1s += 1
-    if N <= 2:
-        if N == 1:
-            if A[st_indx] == '1':
-                res += 1
-        if N == 2:
-            if A[end_indx] == '1':
-                res += 1
-        res_len = max(res,res_len)
-    else:
-        while end_indx <= N-1:
-            length = 0 
-            o_s = 0
-            e1 = st_indx
-            while e1 <= N-1:        
-                if A[e1] != '1':
-                    if o_s < 1:
-                        print("zone",e1)
-                        o_s += 1
+        if N <= 2:                                           # if the array has less than 2 elements, return the number of 1s in the array
+            if N == 1:
+                if A[st_indx] == '1':
+                    res += 1
+            if N == 2:
+                if A[end_indx] == '1':
+                    res += 1
+            res_len = max(res,res_len)
+        else:
+            while end_indx <= N-1:                          # loop through the array to find the longest consecutive 1s
+                length = 0 
+                o_s = 0                                     # o_s is the number of 0's in the current subarray
+                e1 = st_indx                                # e1 is the index of the last 1 in the current subarray
+                length  = 0
+                while e1 <= N-1:            
+                    if A[e1] != '1':                        # if the element is 0, increment the number of 0's in the current subarray
+                        if o_s < 1:                         # if the number of 0's is less than 1, increment the length of the current subarray
+                            o_s += 1
+                            length += 1
+                        else:
+                            break
+                    elif A[e1] == '1':                      # if the element is 1, increment the length of the current subarray
                         length += 1
-                    else:
-                        break
-                elif A[e1] == '1':
-                    length += 1
-                e1 += 1
+                    e1 += 1
+                res_len = max(length,res_len)               # find the longest consecutive 1s in the array
 
+                st_indx  += 1
+                end_indx += 1
 
-            print("st_indx,end_indx",st_indx,end_indx)
-            print("Length:",length)
-            res_len = max(length,res_len)
-
-            st_indx  +=  1
-            end_indx += 1
-
-    if res_len >= no_1s:
-        print("ANSWER:",no_1s)
-    else:
-        print("ANSWER:",res_len)
-
-    print("ANSWER:",res_len)
+        if res_len > no_1s:                                 # if the longest consecutive 1s is greater than the number of 1s in the array, return no_1s
+            return(no_1s)
+        else:
+            return(res_len)                                 # else return the longest consecutive 1s in the array
 
 
 
